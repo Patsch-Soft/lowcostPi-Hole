3951e1621e18a0f6f460d39fda4a207d182f4110
Patsch-Soft <59322704+Patsch-Soft@users.noreply.github.com>
2021-06-30 13:01:31 +0200

    Create qrcode.sh
    Small shell script to display QR-Code in Terminal for wireguard

---
+++ qrcode.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# nur qrcode anzeigen lassen
+echo Zum Anzeigen des QR Code
+echo nachfolgend den Benutzername
+echo eingeben.
+echo
+read -p 'Wireguard Benutzer: ' wguser
+echo
+echo WG-User "$wguser.conf"
+
+qrencode -t UTF8 < ~/"$wguser.conf"
