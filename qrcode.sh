c2c9d21ec622774439cc906ece897ddd074500a2
Patsch-Soft <59322704+Patsch-Soft@users.noreply.github.com>
2021-06-30 13:39:33 +0200

    Update qrcode.sh

--- qrcode.sh
+++ qrcode.sh
@@ -1,3 +1,6 @@
+#Small bash script for QR-Code in Terminal
+
+```
 #!/bin/bash
 # nur qrcode anzeigen lassen
 echo Zum Anzeigen des QR Code
@@ -7,5 +10,5 @@ echo
 read -p 'Wireguard Benutzer: ' wguser
 echo
 echo WG-User "$wguser.conf"
-
 qrencode -t UTF8 < ~/"$wguser.conf"
+```
